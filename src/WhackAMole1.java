412009c0bd5b68ae3f9730f4bdc22b9b61d4247f
INeedAUniqueUsername <alexiscomical445@gmail.com>
2016-05-16 18:30:53 -0700

    WhackAMole
    whack a mole my own

---
+++ src/WhackAMole1.java
@@ -0,0 +1,22 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class WhackAMole1 {
+	public static void main(String[] args) {
+WhackAMole1 mole = new WhackAMole1();
+
+	}
+
+	WhackAMole1() {
+		JFrame frame = new JFrame();
+		JPanel panle = new JPanel();
+		JButton button1 = new JButton();
+		JButton button2 = new JButton();
+		JButton button3 = new JButton();
+		
+		frame.setVisible(true);
+		
+
+	}
+}
