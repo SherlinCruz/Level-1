b11acd0eec6e72a83178bb816141b28c547104e8
Revanty <league@iMac-2.attlocal.net>
2016-08-08 18:33:33 -0700

    August 8, 2016
    Warm Up: Bee
    Exercise: Rain Drop
    Finished both ∆

---
+++ src/Bee88.java
@@ -0,0 +1,20 @@
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
+public class Bee88 {
+
+	static boolean attackTheWasp;
+
+	public static void main(String[] args) {
+		Bee88 bee1 = new Bee88();
+		Bee88 bee2 = new Bee88();
+		Bee88 bee3 = new Bee88();
+
+		bee1.attackTheWasp = true;
+
+		System.out.println(bee1.attackTheWasp);
+		System.out.println(bee2.attackTheWasp);
+		System.out.println(bee3.attackTheWasp);
+
+	}
+
+}
